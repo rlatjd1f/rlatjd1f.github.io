6e116606110969190634840003d3e5cdc2e57daf
ksl930 <dl290382@gmail.com>
2024-11-05 11:23:23 +0900

    prifile update

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,5 +1,5 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faBlog } from '@fortawesome/free-solid-svg-icons';
+// import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../component/profile/IProfile';
@@ -11,13 +11,13 @@ const profile: IProfile.Payload = {
   // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
   image,
   name: {
-    title: 'Tux Linux Penguin',
-    small: '(Tux)',
+    title: '김성리',
+    small: '(Kim Sung Ri)',
   },
   contact: [
     {
-      title: 'tux@linux.org',
-      link: 'mailto:tux@linux.org',
+      title: 'dl290382@gmail.com',
+      link: 'mailto:dl290382@gmail.com',
       icon: faEnvelope,
     },
     {
@@ -26,17 +26,13 @@ const profile: IProfile.Payload = {
       badge: true,
     },
     {
-      link: 'https://github.com/uyu423/resume-nextjs',
-      icon: faGithub,
-    },
-    {
-      link: 'https://www.facebook.com/groups/ubuntu.ko',
-      icon: faFacebook,
+      title: 'https://ksr930.tistory.com/',
+      link: 'https://ksr930.tistory.com/',
+      icon: faBlog,
     },
   ],
   notice: {
-    title:
-      "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
+    title: '연락은 이메일로 부탁드립니다.',
     icon: faBell,
   },
 };
